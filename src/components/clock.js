3c3d334e68233d57ac5dfd5af55da699b7b01c42
lluvia373 <lluvia7531@gmail.com>
2021-09-28 17:11:06 +0900

    blank

--- src/components/clock.js
+++ src/components/clock.js
@@ -15,8 +15,8 @@ class Clock extends React.Component {
        return(
            <div>
                <h1>
-                   {date.getFullYear()}
-                   {date.getMonth() + 1}
+                   {date.getFullYear()}{'  '}
+                   {date.getMonth() + 1}{'  '}
                    {date.getDate()}
                </h1>
                <h2>
@@ -30,7 +30,7 @@ class Clock extends React.Component {
                     }   
                </h2>
                <h2>
-                   {date.getHours() < 10 ? "0" + date.getHours() : date.getHours()}
+                   {date.getHours() < 10 ? "0" + date.getHours() : date.getHours()}{'  '}
                    {date.getMinutes() < 10 ? "0" + date.getMinutes(): date.getMinutes()}
                </h2>
                
